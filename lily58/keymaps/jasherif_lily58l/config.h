384dead4cc21e0a6b338e202e48a499b75e17a97
Jaafer Sheriff <jaafersheriff@gmail.com>
2022-04-24 00:04:22 -0700

    bongo, no led

--- lily58/keymaps/jasherif_lily58l/config.h
+++ lily58/keymaps/jasherif_lily58l/config.h
@@ -31,24 +31,28 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define ENCODER_RESOLUTION 5
 
 #undef RGBLIGHT_ENABLE
+
 #define RGBLIGHT_SLEEP
 
 #ifdef RGB_MATRIX_ENABLE
 #    ifdef RGBLED_NUM
 #        undef RGBLED_NUM
 #    endif
-
 #    define RGBLED_NUM 64  // Number of LEDs
 #    define DRIVER_LED_TOTAL RGBLED_NUM
+
 #    define RGBLED_SPLIT { 29, 35 } 
 #    define RGB_MATRIX_SPLIT RGBLED_SPLIT
 
 #    define RGB_MATRIX_KEYPRESSES                // reacts to keypresses
 #    define RGB_DISABLE_WHEN_USB_SUSPENDED true  // turn off effects when suspended
-#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 255    // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
-#    define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 64    // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
+// #    ifdef RGB_MATRIX_STARTUP_MODE   
+// #       undef RGB_MATRIX_STARTUP_MODE
+// #    endif
+// #    define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_REACTIVE_SIMPLE
 
-// #    define DISABLE_RGB_MATRIX_ALPHAS_MODS
+#    define DISABLE_RGB_MATRIX_ALPHAS_MODS
 #    define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
 #    define DISABLE_RGB_MATRIX_BREATHING
 #    define DISABLE_RGB_MATRIX_BAND_SAT
@@ -73,20 +77,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #    define DISABLE_RGB_MATRIX_TYPING_HEATMAP
 #    define DISABLE_RGB_MATRIX_DIGITAL_RAIN
-// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE
-// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
-// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
-// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
-// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
-// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
-// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE
+#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
 #    define DISABLE_RGB_MATRIX_SPLASH
 #    define DISABLE_RGB_MATRIX_MULTISPLASH
 #    define DISABLE_RGB_MATRIX_SOLID_SPLASH
 #    define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH
 #endif
 
+#define SPLIT_TRANSPORT_MIRROR
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
 // #define SPLIT_USB_DETECT
 // #define NO_USB_STARTUP_CHECK
--- lily58/keymaps/jasherif_lily58l/keymap.c
+++ lily58/keymaps/jasherif_lily58l/keymap.c
@@ -31,6 +31,10 @@ enum custom_keycodes { QWERTY = SAFE_RANGE, LOWER, RAISE, ADJUST, };
 #include "rgb.c"
 #endif
 
+#ifdef OLED_ENABLE
+#include "../bongo.h"
+#endif
+
 extern uint8_t is_master;
 
 #define RAISE MO(_RAISE)
@@ -67,9 +71,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |  F6  |  F7  |  F8  |  F9  |  F10 |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LShift|  F11 |  F12 |      |  -   |  +   |-------.    ,-------|      |      |      |      |      |RShift|
+ * |LShift|  F11 |  F12 |      |  -   |  +   |-------.    ,-------|      |      |RGB_HI|RGB_SI|RGB_VI|RShift|
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |LCTRL |      |      |      |  _   |  =   |-------|    |-------|      | RGB M|      |      |      |Enter |
+ * |LCTRL |      |      |      |  _   |  =   |-------|    |-------|      | RGB M|RGB_HD|RGB_SD|RGB_VD|Enter |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |      | /Space  /       \Space \  |Adjust|  UP  | DOWN |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -78,8 +82,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT( \
  KC_GRAVE, KC_F1,   KC_F2,   KC_F3,   KC_F4,       KC_F5,                     XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, KC_DEL, \
  _______,  KC_F6,   KC_F7,   KC_F8,   KC_F9,       KC_F10,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
- _______,  KC_F11,  KC_F12,  XXXXXXX, KC_MINUS,    S(KC_EQL),                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,\
- _______,  XXXXXXX, XXXXXXX, XXXXXXX, S(KC_MINUS), KC_EQL,  XXXXXXX, XXXXXXX, XXXXXXX, RGB_MOD, RGB_HUI, RGB_SAI, XXXXXXX, _______,\
+ _______,  KC_F11,  KC_F12,  XXXXXXX, KC_MINUS,    S(KC_EQL),                 XXXXXXX, XXXXXXX, RGB_HUI, RGB_SAI, RGB_VAI, KC_RSFT,\
+ _______,  XXXXXXX, XXXXXXX, XXXXXXX, S(KC_MINUS), KC_EQL,  XXXXXXX, XXXXXXX, XXXXXXX, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, _______,\
                               _______, _______,    _______, _______, _______, _______, KC_UP,   KC_DOWN \
 ),
 
@@ -111,11 +115,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------.    ,-------|      |      |RGB ON| HUE+ | SAT+ | VAL+ |
+ * |      |      |      |      |      |      |-------.    ,-------|      |      |      |      |      |      |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |      | MODE | HUE- | SAT- | VAL- |
+ * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Space \  |RAISE | Up   | Down |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -143,234 +147,69 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     return state;
 }
 
-//SSD1306 OLED update loop, make sure to enable OLED_DRIVER_ENABLE=yes in rules.mk
-#ifdef OLED_DRIVER_ENABLE
+//SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
+#ifdef OLED_ENABLE
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    if (is_keyboard_master()) {
-        return OLED_ROTATION_270;
-    } else {
-        return OLED_ROTATION_0;
-    }
+if (!is_keyboard_master())
+    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+  return rotation;
 }
 
-void render_lily58_logo(void) {
-    static const char PROGMEM lily58_logo[] = {
-    // 'logo', 128x32px
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0xc0, 0x40, 0x40, 0xc0, 0x80, 0x80, 0x80, 0x00, 0x00,
-    0x80, 0xe0, 0x70, 0x3c, 0x0e, 0x06, 0x0e, 0x3c, 0x70, 0xe0, 0x80, 0x00, 0x00, 0xc0, 0xc0, 0x00,
-    0xc0, 0xc0, 0x00, 0xc0, 0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xc0, 0xc0, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0xc0, 0x80, 0x00, 0x00, 0x00, 0x80,
-    0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x80, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0x80, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x80, 0x80, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0x80, 0x80, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0xc0, 0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x3f, 0xfc, 0xc0, 0x80, 0x80, 0x80, 0x81, 0x83, 0x83,
-    0x07, 0x07, 0x0c, 0x18, 0x70, 0xe0, 0x80, 0x00, 0x00, 0x01, 0xff, 0xfc, 0x80, 0xb6, 0xb6, 0x80,
-    0xb0, 0xb0, 0x00, 0x36, 0x36, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0, 0xf1, 0x00, 0x00, 0x00, 0x00, 0xff,
-    0xff, 0x00, 0x00, 0x00, 0x30, 0xf0, 0xf0, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0xf0, 0xf0,
-    0x30, 0x00, 0x00, 0x7f, 0xff, 0xff, 0xe1, 0x71, 0x71, 0xf1, 0xf1, 0xe1, 0xc1, 0x81, 0x00, 0x00,
-    0x00, 0x00, 0x0c, 0x3f, 0xff, 0xf3, 0xe1, 0xc1, 0xc1, 0x81, 0x81, 0xc3, 0xff, 0x7f, 0x1c, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x20, 0x70, 0x78, 0xdc, 0xcc, 0x86, 0x06, 0x03, 0x03, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01,
-    0x01, 0x03, 0x02, 0x06, 0x84, 0xe1, 0xfb, 0x38, 0x1c, 0x0c, 0x02, 0x01, 0x01, 0x01, 0x01, 0x01,
-    0x01, 0x01, 0x03, 0x03, 0x06, 0x86, 0xcc, 0xdc, 0x78, 0x70, 0x20, 0x00, 0xff, 0xff, 0x80, 0x80,
-    0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x00, 0x00, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0xff,
-    0xff, 0x80, 0x00, 0x00, 0x00, 0x00, 0x03, 0x1f, 0x7e, 0xf8, 0xe0, 0xf0, 0x7e, 0x1f, 0x03, 0x00,
-    0x00, 0x00, 0x00, 0xe0, 0xe0, 0xc0, 0xc0, 0x80, 0x80, 0x80, 0xc0, 0xe1, 0xff, 0x7f, 0x3f, 0x00,
-    0x00, 0x00, 0x3e, 0xff, 0xff, 0xc1, 0xc0, 0x80, 0x81, 0x81, 0xc3, 0xc3, 0xff, 0xfe, 0x3c, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x03, 0x03, 0x06, 0x06, 0x06, 0x04, 0x04, 0x04, 0x04, 0x06,
-    0x06, 0x02, 0x03, 0x01, 0x01, 0x00, 0x01, 0x01, 0x03, 0x02, 0x06, 0x06, 0x04, 0x04, 0x04, 0x04,
-    0x06, 0x06, 0x06, 0x03, 0x03, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x01, 0x01,
-    0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x00, 0x00, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x01, 0x01, 0x01, 0x00, 0x00, 0x60, 0x60, 0x70, 0x38, 0x1f, 0x0f, 0x03, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x00
-};
-    oled_write_raw_P(lily58_logo, sizeof(lily58_logo));
-}
-
-
-#    define KEYLOG_LEN 6
-char     keylog_str[KEYLOG_LEN] = {};
-uint8_t  keylogs_str_idx        = 0;
-uint16_t log_timer              = 0;
-
-const char code_to_name[60] = {
-    ' ', ' ', ' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f',
-    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-    'R', 'E', 'B', 'T', '_', '-', '=', '[', ']', '\\',
-    '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
-
-void add_keylog(uint16_t keycode) {
-    if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) || (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) {
-        keycode = keycode & 0xFF;
-    }
-
-    for (uint8_t i = KEYLOG_LEN - 1; i > 0; i--) {
-        keylog_str[i] = keylog_str[i - 1];
-    }
-    if (keycode < 60) {
-        keylog_str[0] = code_to_name[keycode];
-    }
-    keylog_str[KEYLOG_LEN - 1] = 0;
-
-    log_timer = timer_read();
-}
-
-void update_log(void) {
-    if (timer_elapsed(log_timer) > 750) {
-        add_keylog(0);
-    }
-}
-
-void render_keylogger_status(void) {
-    oled_write_P(PSTR("KLogr"), false);
-    oled_write(keylog_str, false);
-}
-
-void render_default_layer_state(void) {
-    oled_write_P(PSTR("Layer"), false);
-    oled_write_P(PSTR(" "), false);
-    switch (get_highest_layer(layer_state)) {
-        case _QWERTY:
-            oled_write_P(PSTR("QRTY"), false);
-            break;
-        case _LOWER:
-            oled_write_ln_P(PSTR("LOW"), false);
-            break;
-        case _RAISE:
-            oled_write_P(PSTR("HIGH"), false);
-            break;
-        case _ADJUST:
-            oled_write_ln_P(PSTR("ADJ"), false);
-            break;
-        default:
-            oled_write_ln_P(PSTR("Undefined"), false);
-    }
-}
-
-void render_rgb_matrix(void) {
-    oled_write_P(PSTR("RGB"), false);
-    oled_write_P(PSTR(" "), false);
-    if (rgb_matrix_is_enabled()) {
-        switch(rgb_matrix_get_mode()) {
-#define _MYCASE(x) case x: oled_write_P(PSTR(#x), false); break
-            _MYCASE(RGB_MATRIX_SOLID_REACTIVE_SIMPLE);
-            _MYCASE(RGB_MATRIX_SOLID_REACTIVE);
-            _MYCASE(RGB_MATRIX_SOLID_REACTIVE_WIDE);
-            _MYCASE(RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE);
-            _MYCASE(RGB_MATRIX_SOLID_REACTIVE_CROSS);
-            _MYCASE(RGB_MATRIX_SOLID_REACTIVE_MULTICROSS);
-            _MYCASE(RGB_MATRIX_SOLID_REACTIVE_NEXUS);
-            _MYCASE(RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS);
-            default:
-                oled_write_P(PSTR("idk"), false);
-                break;
-        }
-    }
-    else {
-        oled_write_P(PSTR("Disabled"), false);
-    }
-}
-
-void render_keylock_status(led_t led_state) {
-    oled_write_ln_P(PSTR("Lock"), false);
-    oled_write_P(PSTR(" "), false);
-    oled_write_P(PSTR("N"), led_state.num_lock);
-    oled_write_P(PSTR("C"), led_state.caps_lock);
-    oled_write_ln_P(PSTR("S"), led_state.scroll_lock);
-}
-
-void render_mod_status(uint8_t modifiers) {
-    oled_write_ln_P(PSTR("Mods"), false);
-    oled_write_P(PSTR(" "), false);
-    oled_write_P(PSTR("S"), (modifiers & MOD_MASK_SHIFT));
-    oled_write_P(PSTR("C"), (modifiers & MOD_MASK_CTRL));
-    oled_write_P(PSTR("A"), (modifiers & MOD_MASK_ALT));
-    oled_write_P(PSTR("G"), (modifiers & MOD_MASK_GUI));
-}
-
-void render_status_main(void) {
-    // Show keyboard layout
-    render_default_layer_state();
-    // Add a empty line
-    oled_write_P(PSTR("-----"), false);
-    // Show host keyboard led status
-    render_keylock_status(host_keyboard_led_state());
-    // Add a empty line
-    oled_write_P(PSTR("-----"), false);
-    // Show modifier status
-    render_mod_status(get_mods());
-    // Add a empty line
-    oled_write_P(PSTR("-----"), false);
-    render_keylogger_status();
-}
-
-void oled_task_user(void) {
-  update_log();
+bool oled_task_user(void) {
+  // update_log();
   if (is_keyboard_master()) {
-    // render_status_main();  // Renders the current keyboard state (layer, lock, caps, scroll, etc)
-    render_rgb_matrix();
+    return false;
   } else {
-    render_rgb_matrix();
-    // render_lily58_logo();
+    // oled_write(read_logo(), false);
+    draw_bongo();
   }
+  return false;
 }
-#endif // OLED_DRIVER_ENABLE
+#endif // OLED_ENABLE
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-#ifdef OLED_DRIVER_ENABLE
     if (record->event.pressed) {
-        add_keylog(keycode);
     }
-#endif // OLED_DRIVER_ENABLE
-
-
-    switch (keycode) {
-        case LOWER:
-            if (record->event.pressed) {
-                layer_on(_LOWER);
-            } else {
-                layer_off(_LOWER);
-            }
-            return false;
-        case RAISE:
-            if (record->event.pressed) {
-                layer_on(_RAISE);
-            } else {
-                layer_off(_RAISE);
-            }
-            return false;
-        case ADJUST:
-            if (record->event.pressed) {
-                layer_on(_ADJUST);
-            } else {
-                layer_off(_ADJUST);
-            }
-            return false;
-
-#if defined(RGBLIGHT_ENABLE) || defined(RGB_MATRIX_ENABLE)
-        case RGB_TOG:
-             /* Override layer-based RGB and resume RGB effect to be able to preview changes */
-             if (record->event.pressed) {
-                 rgb_matrix_toggle();
-                 save_rgb_config();
-             }
-             return false;
-        case RGB_MOD:
-             /* Override layer-based RGB and resume RGB effect to be able to preview changes */
-             if (record->event.pressed) {
-                 rgb_matrix_step();
-                 save_rgb_config();
-             }
-             return false;
-
+//     switch (keycode) {
+//         case LOWER:
+//             if (record->event.pressed) {
+//                 layer_on(_LOWER);
+//             } else {
+//                 layer_off(_LOWER);
+//             }
+//             return false;
+//         case RAISE:
+//             if (record->event.pressed) {
+//                 layer_on(_RAISE);
+//             } else {
+//                 layer_off(_RAISE);
+//             }
+//             return false;
+//         case ADJUST:
+//             if (record->event.pressed) {
+//                 layer_on(_ADJUST);
+//             } else {
+//                 layer_off(_ADJUST);
+//             }
+//             return false;
+// 
+// #if defined(RGBLIGHT_ENABLE) || defined(RGB_MATRIX_ENABLE)
+//         case RGB_TOG:
+//              /* Override layer-based RGB and resume RGB effect to be able to preview changes */
+//              if (record->event.pressed) {
+//                  rgb_matrix_toggle();
+//                  save_rgb_config();
+//              }
+//              return false;
+//         case RGB_MOD:
+//              /* Override layer-based RGB and resume RGB effect to be able to preview changes */
+//              if (record->event.pressed) {
+//                  rgb_matrix_step();
+//                  save_rgb_config();
+//              }
+//              return false;
+// 
 //         case RGB_HUI:
 //         case RGB_HUD:
 //         case RGB_SAI:
@@ -395,8 +234,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 //                 save_rgb_config();
 //              }
 //              return false;
- #endif
-    }
+//  #endif
+//     }
     return true;
 }
 
